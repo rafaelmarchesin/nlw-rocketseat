36487913674405670ba384f33f70a1b691aca473
Rafael Marchesin <rmarchesin@gmail.com>
2020-08-06 23:27:38 -0300

    feat: Frontend Página Aluno

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Imagens
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
+
+//Ícones
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveStudyIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+//Estilos
+import './styles.css';
 
 function Landing() {
     return (
@@ -21,18 +29,18 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="#" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="#" className="study">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveStudyIcon} alt="Estudar"/>
-                        Estudar
-                    </a>
+                        Dar aulas
+                    </Link>
                 </div>
 
-                <span className="total-conections">
+                <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
 
